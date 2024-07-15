74b2e3e12e6db2d78940786633d9a368e92dcf1b
Junior Sierra Gomez <junior_sierra97@outlook.com>
2024-07-15 15:22:18 -0500

    Project init with plugin

---
+++ applications/app-service/src/main/java/go/jrsierra/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package go.jrsierra.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "go.jrsierra.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
